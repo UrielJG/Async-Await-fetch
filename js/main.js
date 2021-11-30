b50b45ee8ec278e8f4e33f819a2fd485b11f5d48
UrielJG <90708434+UrielJG@users.noreply.github.com>
2021-11-30 11:52:28 -0600

    Primer versión

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let RegistrosHTML = document.getElementById('RegistrosHTML');
+let arrayRegistros = [];
+
+async function leeRegistros(){
+
+    const respuesta = await fetch("data/registros.json");
+    const registros = await respuesta.json();    
+        
+    return registros;
+}
+
+async function muestraRegistros(){
+    try {
+        arrayRegistros = await leeRegistros();
+    } catch(e){
+        console.log("Error generado: " + e);
+    }
+    console.log(arrayRegistros); 
+    arrayRegistros.forEach(function(item, index){
+        RegistrosHTML.innerHTML = RegistrosHTML.innerHTML + 
+        item.nombre + " " + 
+        item.apellido + " -> " + 
+        item.telefono + "<br>";  
+    });
+    //RegistrosHTML.innerHTML = RegistrosHTML.innerHTML + arrayRegistros.nombre + " " + arrayRegistros.apellido;  
+}
+
+muestraRegistros();
